1601dc8185fb3af9fb4aa01c04afb7a4b2faae00
Max <100712759+mbgl20@users.noreply.github.com>
2025-12-11 13:04:47 +0100

    Create s.sql

---
+++ s.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `un` varchar(50) NOT NULL,
+  `dn` varchar(100) NOT NULL,
+  `pw` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `un` (`un`);
